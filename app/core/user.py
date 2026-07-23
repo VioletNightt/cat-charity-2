10f84b0e75f7bd899dd8a55886a19785deccc4fb
Александр Харо <alexkharo@yandex.ru>
2026-07-24 00:04:36 +0300

    3

--- app/core/user.py
+++ app/core/user.py
@@ -2,13 +2,17 @@ from typing import AsyncGenerator
 
 from fastapi import Depends, Request
 from fastapi_users import BaseUserManager, FastAPIUsers, exceptions
-from fastapi_users.authentication import (AuthenticationBackend,
-                                          BearerTransport, JWTStrategy)
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    BearerTransport,
+    JWTStrategy,
+)
 from fastapi_users.manager import IntegerIDMixin
 from fastapi_users_db_sqlalchemy import SQLAlchemyUserDatabase
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.config import settings
+from app.core.constants import JWT_TOKEN_LIFETIME_SECONDS, MIN_PASSWORD_LENGTH
 from app.core.db import get_async_session
 from app.models.user import User
 from app.schemas.user import UserCreate
@@ -27,9 +31,10 @@ class UserManager(IntegerIDMixin, BaseUserManager[User, int]):
     async def validate_password(
         self, password: str, user: UserCreate
     ) -> None:
-        if len(password) < 3:
+        if len(password) < MIN_PASSWORD_LENGTH:
             raise exceptions.InvalidPasswordException(
-                reason="Password should be at least 3 characters"
+                reason=f"""Password should be at
+                least {MIN_PASSWORD_LENGTH} characters"""
             )
         if user.email in password:
             raise exceptions.InvalidPasswordException(
@@ -52,7 +57,8 @@ bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
 
 
 def get_jwt_strategy() -> JWTStrategy:
-    return JWTStrategy(secret=settings.secret, lifetime_seconds=3600)
+    return JWTStrategy(secret=settings.secret,
+                       lifetime_seconds=JWT_TOKEN_LIFETIME_SECONDS)
 
 
 auth_backend = AuthenticationBackend(
--- app/main.py
+++ app/main.py
@@ -2,10 +2,8 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.api.routes import charity_project, donation
+from app.api.routers import main_router
 from app.core.db import Base, engine
-from app.core.user import auth_backend, fastapi_users
-from app.schemas.user import UserCreate, UserRead, UserUpdate
 
 
 @asynccontextmanager
@@ -19,26 +17,7 @@ app = FastAPI(
     lifespan=lifespan,
     title="QRKot",
     description="Фонд поддержки котиков",
-    version="1.0.0"
+    version="1.0.0",
 )
 
-app.include_router(charity_project.router,
-                   prefix="/charity_project", tags=["Charity Projects"])
-app.include_router(donation.router, prefix="/donation", tags=["Donations"])
-app.include_router(
-    fastapi_users.get_auth_router(auth_backend),
-    prefix="/auth/jwt",
-    tags=["auth"],
-)
-app.include_router(
-    fastapi_users.get_register_router(UserRead, UserCreate),
-    prefix="/auth",
-    tags=["auth"],
-)
-
-users_router = fastapi_users.get_users_router(UserRead, UserUpdate)
-users_router.routes = [
-    route for route in users_router.routes
-    if route.name not in {"users:delete_user", "users:delete_current_user"}
-]
-app.include_router(users_router, prefix="/users", tags=["users"])
+app.include_router(main_router)
